d6ef7578f9935544a9ea33c2469d897fbaf558f7
xnkasy <xnkasypriyanshu@gmail.com>
2021-03-30 01:39:52 +0530

    Added adding a song, but duplicate artists, albums, or even songs not handled

--- queryC.py
+++ queryC.py
@@ -1,5 +1,5 @@
 from codeval import code2val, pcode2val, lcode2val, tcode2val
-
+from extract import getAttributes
 def queryCreatorHelper(form, where_head):
     song = form.get("Song")
     if(not ( song is None or  not song)):
@@ -65,3 +65,36 @@ def DelQueryCreatorName(form):
 def DelQueryCreatorLink(form):
     song = form.get("Link")
     return "delete from song where link = "+song
+
+def valueCreator(attrlist):
+    ret = ""
+    ret = ret + str(attrlist)[1:-1] +");"
+    return ret
+def InsQueryCreatorLink(form):
+    link = form.get("Link")
+    id = link.split("/")[-1]
+    song_db, song_art_db, alb_art_db, album_db, artist_db, genre_db = getAttributes(id)
+    print(song_db)
+    #song insert
+    song_query = "Insert into song values (" + valueCreator(song_db)
+    #artist insert
+    artist_query = ""
+    for a in artist_db:
+        artist_query =  artist_query + "Insert into artist values ("+valueCreator(a) +"\n"
+    #album insert
+    album_query = "Insert into album values ("+valueCreator(album_db)
+    #song_artist insert
+    song_art_query = ""
+    for atlist in song_art_db:
+        song_art_query = song_art_query + "Insert into artist_song values (" + valueCreator(atlist)+"\n"
+    #album_artist insert
+    album_art_query = ""
+    for atlist in alb_art_db:
+        album_art_query = album_art_query + "Insert into album_artist values (" + valueCreator(atlist)+"\n"
+    #genre_db insert
+    art_genre_query = ""
+    for atlist in genre_db:
+        art_genre_query = art_genre_query + "Insert into artist_genre values (" + valueCreator(atlist)+"\n"
+    return "BEGIN;" + "\n" + artist_query + album_query+ "\n" + song_query + "\n" + song_art_query + album_art_query + art_genre_query +"COMMIT;"
+
+    
