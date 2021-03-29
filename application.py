7969f9218eb2e57905d5a6ac885beacdf7fc0bd1
xnkasy <xnkasypriyanshu@gmail.com>
2021-03-30 02:14:28 +0530

    Added add song feature to application

--- application.py
+++ application.py
@@ -1,18 +1,18 @@
 from flask import Flask, render_template, request
-from queryC import queryCreator
+from queryC import queryCreator, InsQueryCreatorLink
 import psycopg2 
 import sys
 
 app = Flask(__name__)
 
-app.config['dbname'] = "db" #sys.argv[1]
+app.config['dbname'] = "sample" #sys.argv[1]
 app.config['user'] = "postgres" #sys.argv[2]
 dbname = app.config.get('dbname')
 user = app.config.get('user')
-# app.config['password'] = "qmwnebrv1234" #sys.argv[3]
-# password = app.config.get('password')
-# connect = ("dbname="+dbname+ " user="+user+ " password="+password)
-connect = ("dbname="+dbname+ " user="+user)
+app.config['password'] = "qmwnebrv1234" #sys.argv[3]
+password = app.config.get('password')
+connect = ("dbname="+dbname+ " user="+user+ " password="+password)
+# connect = ("dbname="+dbname+ " user="+user)
 print(connect)
 conn = psycopg2.connect(connect)
 cur = conn.cursor()
@@ -42,6 +42,10 @@ def addalbum():
 
 @app.route("/addsuccess", methods=["POST"])
 def addsuccess():
+    print("Here we are")
+    command = InsQueryCreatorLink(request.form)
+    print(command)
+    cur.execute(command)
     return render_template("addsuccess.html")
 
 @app.route("/addfailure", methods=["POST"])
